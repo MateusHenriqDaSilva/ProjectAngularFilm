9434ce53324cf75686e9d797d68ddf87206005cb
mateusprogaming <mateus_silvah@hotmail.com>
2023-02-06 03:53:12 -0300

    firt

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,30 +8,31 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  username: string = ""
-  password: string = ""
-  errorMsg: string = ""
+
+  username = "";
+  password = "";
+  errorMsg = "";
 
   constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-
   }
 
   login() {
     if (this.username.trim().length === 0) {
-      this.errorMsg = "Usrename is required"
+      this.errorMsg = "Username is required";
     } else if (this.password.trim().length === 0) {
-      this.errorMsg = 'Password is required'
+      this.errorMsg = "Password is required";
     } else {
-      this.errorMsg = ""
-      let res = this.auth.login(this.username, this.password)
-      if(res === 200){
-        this.router.navigate(['home'])
+      this.errorMsg = "";
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home']);
       }
-      if(res === 403){
-        this.errorMsg = "Invalid Credenciais"
+      if (res === 403) {
+        this.errorMsg = "Invalid Credentials";
       }
     }
   }
+
 }
